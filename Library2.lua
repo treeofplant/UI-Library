48f5cdcbbdc6749a4e07a5732dfaa420ef0bb4fb
hu <77543293+treeofplant@users.noreply.github.com>
2023-06-17 13:52:29 +0400

    Update Library2.lua

--- Library2.lua
+++ Library2.lua
@@ -1,7 +1,7 @@
 if game.CoreGui:FindFirstChild("LibraryHub05481") then
   game.CoreGui.LibraryHub05481:Destroy()
 end
---n
+--nw
 function createHub(namee)
 local hub = Instance.new("ScreenGui")
 local MainGUI = Instance.new("Frame")
